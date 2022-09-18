363ed533d6cde79f9432c34fa3722b46e720b847
Anishaverma <55661815+Anishaverma@users.noreply.github.com>
2022-09-19 00:01:48 +0530

    remove pagination errors

--- src/Components/MoviePage/MoviePage.jsx
+++ src/Components/MoviePage/MoviePage.jsx
@@ -5,19 +5,6 @@ import "./MoviePage.css";
 
 import { API_URL, API_KEY } from "../../secret";
 
-// class MoviePage extends Component {
-//   state = {
-//     videoObject: {},
-//   };
-
-
-
-//   render() {
-
-
-
-//   }
-// }
 
 const MoviePage = (props) => {
   const [videoObject, setVideoObject] = useState({});
@@ -31,20 +18,17 @@ const MoviePage = (props) => {
   let { title, tagline, vote_average, poster_path, overview } = props.location.state;
   useEffect(async () => {
     let response = await axios.get(
-      `${API_URL}/movie/${props.location.state.id}/videos?api_key=${API_KEY}&language=en-US`
+      `${API_URL}/movie/${props?.location?.state?.id}/videos?api_key=${API_KEY}&language=en-US`
     ); 
-    console.log(response);
-    let videoObject = response.data.results.filter((videoObj) => {
-      if (videoObj.type == "Trailer" && videoObj.site == "YouTube") {
+    console?.log(response);
+    let videoObject = response?.data?.results?.filter((videoObj) => {
+      if (videoObj?.type == "Trailer" && videoObj?.site == "YouTube") {
         return true;
       }
       return false;
     });
-    // [{}]
     setVideoObject(videoObject[0],)
-    //  this.setState({
-    //   videoObject: videoObject[0],
-    // });
+
 
 
   }, [])
@@ -64,7 +48,7 @@ const MoviePage = (props) => {
           <p>{overview}</p>
         </div>
         <div className="movie-trailer">
-          <YouTube videoId={videoObject.key} opts={opts}></YouTube>
+          <YouTube videoId={videoObject?.key} opts={opts}></YouTube>
         </div>
       </div>
     </div>
